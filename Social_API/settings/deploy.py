caf7578ef7a6d271896d7cb58bec27ee3ff08cfa
kazi0021 <kazi0021@d.umn.edu>
2021-09-11 22:13:52 -0500

    feature: Added deployment settings

--- Social_API/settings/deploy.py
+++ Social_API/settings/deploy.py
@@ -1,13 +1,14 @@
 from .base import *
-import environ
-env = environ.Env()
-env.read_env(env.str('ENV_PATH', BASE_DIR+'/.env'))
+from dotenv import load_dotenv
+import os
+
+load_dotenv(f'{BASE_DIR}/.env')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-0rkqr)ui_ag6b15jupm@6tjx2jyw4dkkw6a@%(kqmv+97%ve-v-new'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', ]
 
@@ -26,5 +27,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env('EMAIL')
-EMAIL_HOST_PASSWORD = env('EMAIL_APP_PASSWORD')
+EMAIL_HOST_USER = os.getenv('EMAIL')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_APP_PASSWORD')
--- Social_API/wsgi.py
+++ Social_API/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Social_API.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Social_API.settings.deploy')
 
 application = get_wsgi_application()
