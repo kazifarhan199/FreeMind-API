59ff89d8fad6c3b5d6d765331fac103332f62583
kazi0021 <kazi0021@d.umn.edu>
2022-02-11 16:17:11 -0600

    change: Deployment using same database as deveelopement

--- src/Social_API/settings/deploy.py
+++ src/Social_API/settings/deploy.py
@@ -18,7 +18,7 @@ ALLOWED_HOSTS = ['*', ]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db_production.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
