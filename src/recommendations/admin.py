805c85527938498f7354d76024348fc7e493bd8b
kazi0021 <kazi0021@d.umn.edu>
2022-03-20 19:53:21 -0500

    feature: Added tracker to admin

--- src/recommendations/admin.py
+++ src/recommendations/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Labels, Ratings
+from .models import Labels, Ratings, Tracker
 
 admin.site.register(Labels)
 admin.site.register(Ratings)
+admin.site.register(Tracker)
