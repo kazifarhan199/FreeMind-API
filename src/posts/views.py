a2e29f774e4b91903faa0a3109d67a2ecb4c343b
kazi0021 <kazi0021@d.umn.edu>
2022-06-21 15:49:58 +0530

    debug: unable to post

--- src/posts/views.py
+++ src/posts/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from groups.models import GroupsMember
+from groups.models import GroupsMember, Groups
 from groups.permissions import IsInGroup
 from .pagination import PostPageNumberPagination, PostPageNumberPagination1000
 from .models import Post, PostComment, PostLike
