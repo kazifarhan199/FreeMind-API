ff5a2811768f87ddce7198418ec9c7b6f2685d84
Farhanuddin Kazi <kazifarhan199@gmail.com>
2022-11-20 11:07:02 -0600

    feature: Added default_profile and changed default image path

--- src/accounts/signals.py
+++ src/accounts/signals.py
@@ -2,7 +2,7 @@ from .models import Profile, OTP
 
 def create_profile(sender, instance, **kwargs):
     if not Profile.objects.filter(user=instance).exists():
-        Profile.objects.create(user=instance, image='media/image/default_profile.jpg')
+        Profile.objects.create(user=instance, image='image/default_profile.jpg')
 
 def delete_existing_otp(sender, instance, **kwargs):
     if OTP.objects.filter(email=instance.email).exists():
