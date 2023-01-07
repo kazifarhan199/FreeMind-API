2236b6cce7ae5549d6719b3b82f3cf15bc0db674
kazi0021 <kazi0021@d.umn.edu>
2023-01-07 11:57:14 -0600

    refactored: Refactored notifications signal

--- src/notifications/signals.py
+++ src/notifications/signals.py
@@ -7,48 +7,46 @@ from accounts.models import Device
 
 
 def sendNotifications(sender, instance, created, **kwargs):
-    devices = Device.objects.filter(user=instance.user)
-
     if instance.survey:
         sendSurveyNotifications(sender, instance, created, **kwargs)
         return 
     else:
         sendPostNotifications(sender, instance, created, **kwargs)
-        return 
+        return
 
 
 def sendPostNotifications(sender, instance, created, **kwargs):
     devices = Device.objects.filter(user=instance.user)
     if instance.post_like != None:
         data = {
-                    "click_action":"FLUTTER_NOTIFICATION_CLICK",
-                    "notification_id": instance.id,
-                    "title": instance.text, 
-                    "body": "",
-                    "post": str(instance.post.id),
-                }
+            "click_action":"FLUTTER_NOTIFICATION_CLICK",
+            "notification_id": instance.id,
+            "title": instance.text, 
+            "body": "",
+            "post": str(instance.post.id),
+        }
     elif instance.post_comment != None:
         data = {
-                    "click_action":"FLUTTER_NOTIFICATION_CLICK",
-                    "notification_id": instance.id,
-                    "title": instance.text, 
-                    "body": instance.post_comment.text,
-                    "post": str(instance.post.id),
-                }
+            "click_action":"FLUTTER_NOTIFICATION_CLICK",
+            "notification_id": instance.id,
+            "title": instance.text, 
+            "body": instance.post_comment.text,
+            "post": str(instance.post.id),
+        }
     elif instance.post != None:
         data = {
-                    "click_action":"FLUTTER_NOTIFICATION_CLICK",
-                    "notification_id": instance.id,
-                    "title": instance.text, 
-                    "body": instance.post.title,
-                    'post':str(instance.post.id),
-                }
+            "click_action":"FLUTTER_NOTIFICATION_CLICK",
+            "notification_id": instance.id,
+            "title": instance.text, 
+            "body": instance.post.title,
+            'post':str(instance.post.id),
+        }
 
-    for d in devices:
-        re = requests.post(
+    for device in devices:
+        response = requests.post(
             'https://fcm.googleapis.com/fcm/send', 
             data = json.dumps({
-                "to": d.devicetoken,
+                "to": device.devicetoken,
                 "mutable_content" : True,
                 "content_available": True,
                 "data": data,
@@ -61,28 +59,26 @@ def sendPostNotifications(sender, instance, created, **kwargs):
             }
         )
         
-
-
-        if json.loads(re.text)['results'][0].get('error')=='NotRegistered':
-            d.delete()
+        if json.loads(response.text)['results'][0].get('error')=='NotRegistered':
+            device.delete()
 
 
 def sendSurveyNotifications(sender, instance, created, **kwargs):
     devices = Device.objects.filter(user=instance.user)
     data = {
-                "click_action":"FLUTTER_NOTIFICATION_CLICK",
-                "notification_id": instance.id,
-                "title": instance.text, 
-                "body": "",
-                "post": 0,
-                "survey": True
-            }
+        "click_action":"FLUTTER_NOTIFICATION_CLICK",
+        "notification_id": instance.id,
+        "title": instance.text, 
+        "body": "",
+        "post": 0,
+        "survey": True
+    }
 
-    for d in devices:
-        re = requests.post(
+    for device in devices:
+        response = requests.post(
             'https://fcm.googleapis.com/fcm/send', 
             data = json.dumps({
-                "to": d.devicetoken,
+                "to": device.devicetoken,
                 "mutable_content" : True,
                 "content_available": True,
                 "data": data,
@@ -95,5 +91,5 @@ def sendSurveyNotifications(sender, instance, created, **kwargs):
             }
         )
 
-        if json.loads(re.text)['results'][0].get('error')=='NotRegistered':
-            d.delete()
+        if json.loads(response.text)['results'][0].get('error')=='NotRegistered':
+            device.delete()
