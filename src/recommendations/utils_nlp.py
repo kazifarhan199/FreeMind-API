68a5da4e655276aa19d74209ac6f5bbc757974cc
Farhanuddin Kazi <kazifarhan199@gmail.com>
2023-01-22 23:15:27 -0600

    feature: Not loading NLP model in DEBUG mode

--- src/recommendations/utils_nlp.py
+++ src/recommendations/utils_nlp.py
@@ -2,22 +2,27 @@ from django.conf import settings
 from simpletransformers.classification import ClassificationModel, ClassificationArgs
 import numpy as np
 
-model_args = ClassificationArgs(
-    num_train_epochs=1,
-    overwrite_output_dir=True,
-    evaluate_during_training=True,
-)
-model = ClassificationModel(
-    settings.MODEL_NAME, settings.MODELS_PATH
-)
+if not settings.DEBUG:
+    model_args = ClassificationArgs(
+        num_train_epochs=1,
+        overwrite_output_dir=True,
+        evaluate_during_training=True,
+    )
+    model = ClassificationModel(
+        settings.MODEL_NAME, settings.MODELS_PATH
+    )
 
-pred_label = {0:'Exercise', 1:'Food', 2:'general', 3: 'Stress'}
+    pred_label = {0:'Exercise', 1:'Food', 2:'general', 3: 'Stress'}
 
-def get_nlp_classification(text):
-    text_list = [text]
-    predictions, raw_outputs = model.predict(text_list)
-    prediction = predictions[0]
+    def get_nlp_classification(text):
+        text_list = [text]
+        predictions, raw_outputs = model.predict(text_list)
+        prediction = predictions[0]
 
-    label = pred_label[prediction]
+        label = pred_label[prediction]
 
-    return label, raw_outputs
+        return label, raw_outputs
+
+else:
+    def get_nlp_classification(text):
+        return 'general', (0, 0, 0)
