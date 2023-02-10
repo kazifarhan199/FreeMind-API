d4b75131767a2535fea575cc7e0f801deba0785a
kazi0021 <kazi0021@d.umn.edu>
2023-02-10 11:53:05 -0600

    feature: Added code to load labels from csv file

---
+++ src/recommendations/utils_labelloader.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import requests
+
+url = 'http://localhost:59000/recommendations/label-create/'
+
+def load_data():
+    file_path = input("Enter the path to the file to load: ")
+    df = pd.read_csv(file_path)
+    df = df.fillna('.')
+    for row in df.values:
+        data = {
+            'name' : row[0],
+            'type' : row[1],
+            'reason' : row[2],
+            'link' : row[3],
+            'is_label' : row[4],
+            'is_coupuled' : row[5],
+            'source_based' : row[6],
+            'social_based' : row[7],
+            'hybrid_based' : row[8],
+        }
+        resp = requests.post(url, json = data)
+        print(resp, '--', resp.text)
