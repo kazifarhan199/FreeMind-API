0a3d482b3f3bfa62c0584449e72d727b1167e329
kazi0021 <kazi0021@d.umn.edu>
2023-02-14 11:49:03 -0600

    feature: Added username and title search in admin for posts

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Post, PostImages, PostLike, PostComment, CommentFeedback
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['user__username','title']
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(PostImages)
 admin.site.register(PostLike)
 admin.site.register(PostComment)
