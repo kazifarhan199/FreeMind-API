763eeeff31c53afaa22dd7305a2d3fe740c157dc
putta012 <putta012@d.umn.edu>
2023-02-20 21:38:00 -0600

    Feature: Data analysis function is created but all the stress qualifiers are not yet added as 'stressfull'  and 'balanced' are the only  known qualifiers

--- src/wearable/utils.py
+++ src/wearable/utils.py
@@ -58,7 +58,7 @@ def dataToDailies(data):
                 for i in filter:
                     i.is_active=False
                     i.save()
-            #doing a create or update so that if you receive same day data it can just be updated istead of creating a new record
+            #create a instance in the DB
             instance = models.DailyData.objects.create(
                 
                 userId = day.get('userId'),
@@ -88,7 +88,14 @@ def dataToDailies(data):
             instance.save()
             # before calling the function what if both sleep and daily have new data at the same time
             # how will that be handled
-            #dataAnalysis(fromDaily=True)
+            
+        #this block will filter the latest data of the user in question and makes a call to the analysis function
+        filter= models.DailyData.objects.filter(is_active=True, userId = day.get('userId'),userAccessToken=day.get('userAccessToken'),calendarDate=day.get('calendarDate'))
+        if(filter.exists()):
+            for curr_obj in filter:
+                dataAnalysis(curr_obj, _type='daily')
+            
+            
     except Exception as e:
         # handle the exception
         print(f'Error saving data: {e}') 
@@ -105,8 +112,7 @@ def dataToSleep(data):
                 for i in filter:
                     i.is_active=False
                     i.save()
-            #doing a create or update so that if you receive same day data it can just be updated istead of creating a new record
-            # Create or retrieve the instance from the db
+            # Create a instance in the db
             instance = models.SleepData.objects.create(               
                 userId = sleep.get('userId'),
                 userAccessToken=sleep.get('userAccessToken'),
@@ -120,38 +126,81 @@ def dataToSleep(data):
             instance.save()
             # before calling the function what if both sleep and daily have new data at the same time
             # how will that be handled
-            #dataAnalysis(fromDaily=False)
+            
     except Exception as e:
         # handle the exception
         print(f'Error saving data: {e}') 
 
 # the function Does the data analysis before trying to alter the recommendation list
-'''
-def dataAnalysisDaily():
-    # what are the five activity types? exercise, food, 
-    activityType=Null
-    # logic to do the analysis
-    return activityType
 
-def dataAnalysisSleep():
-    activityType=Null
-    # logic to do the analysis
-    for sleep return Rest/Sleep if user need to rest otherwise Null
-    return activityType
+#return multiplicity 
+    #1 if activity is in range
+    #0.1-0.9: maybe 0.7: if activity level is already high you don't want to suggest the user to do more of the activity
+    #1.1-1.9: maybe 1.3: if activity level is low you want to suggest the user to do more of the activity
+def dataAnalysisDaily(curr_obj_data): 
+    reason = ""
+    dict_ActivityMultiplicity ={ 'exercise':None, 'food':None, 'stress':None, 'general':1, 'sleep':None}
+
+    # logic to do the Analysis
+
     
-def dataAnalysis(fromDaily=True):
-    
-    if(fromDaily):
-        # see if current day's data has null feilds for all, otherwise call the daily Analysis
-        dataAnalysisDaily()
+    #STEPS and CALORIES Analysis
+    if((curr_obj_data.steps==None or curr_obj_data.steps==0) and (curr_obj_data.activeKilocalories==0 or curr_obj_data.activeKilocalories==None)):
+        reason = reason + "Your calories and steps have not been measured. "
     else:
-        # see if current day's data has null feilds for all, otherwise call the sleep Analysis
-        dataAnalysisSleep()
-    # setup a trigger to do analysis when there's new data in DB
+        if(curr_obj_data.activeKilocalories >= 300 or (curr_obj_data.steps>= 2000 and curr_obj_data.steps<=5000)):
+            dict_ActivityMultiplicity['exercise']=0.9
+            dict_ActivityMultiplicity['food']=1.1
+            reason = reason + "You have done enough exercise for the day, consume nutritous food. "
+        elif(curr_obj_data.activeKilocalories >= 500 or curr_obj_data.steps> 5000 ):
+            dict_ActivityMultiplicity['exercise']=0.7
+            dict_ActivityMultiplicity['food']=1.3
+            reason = reason + "You have perfored high levels of exercise, consume nutriouse food to gain the benefit of exercising "
 
+    # STRESS Analysis 
+    dictStressQualifiers = {'stressfull':1.3, 'unknown':1, 'balanced': 1} 
+    if(curr_obj_data.stressQualifier == "unknown"):
+        reason = reason + "Stress is not measured, please make sure the watch is on your wrist all the time to ensure right measurements. "
+    else:
+        reason = reason + "Your stress level is analysed as " + curr_obj_data.stressQualifier + ". "
+    dict_ActivityMultiplicity['stress']=dictStressQualifiers[curr_obj_data.stressQualifier]
+
+    return dict_ActivityMultiplicity, reason
+
+
+
+def dataAnalysisSleep(curr_obj_data):
+    filter= models.SleepData.objects.filter(is_active=True, userId = curr_obj_data.userId,userAccessToken=curr_obj_data.userAccessToken,calendarDate=curr_obj_data.calendarDate)
+    reason = ""
+    if(filter.exists()):
+        if(filter.first().durationInSeconds >= 25200):
+            if(filter.first().overallSleepScorequalifierKey != "unknown"):
+                reason = reason + "You had enough sleep and the quality of sleep is " +  filter.first().overallSleepScorequalifierKey + ". "
+            else:
+                reason = reason + "You had enough sleep. "
+        else:
+            reason = reason + "Also you should take rest as you had less than 7 hours of sleep. "
+    else:
+        reason = reason + "Sleep is not measured. "
+    
+    return reason
+
+
+#think about how to return the reason?
+# the analysis function is called only when there is new data in the table, this is made sure by making th call analysis in dataToSleep/dataToDaily after a new instance is saved.    
+# what are the five activity types? exercise, food, stress and general
+
+def dataAnalysis(curr_obj_data, _type):
+    if(_type=='daily'):
+        dict, reason = dataAnalysisDaily(curr_obj_data)
+        reason = reason + dataAnalysisSleep(curr_obj_data)
+        print(reason, dict.keys(), dict.values())
+    #communicate to recommendation system
+    
+    # create 1-5/0.1-1.9 scale for each activity and then increase or decrease the multiplicty of the type accordingly
     # Analyse the current day's data to come up with a activity type to increse/decrease the prominance
 
-'''
+
 
 # Set the schedule for the task to run every 2 hours
 # app.conf.beat_schedule = {
